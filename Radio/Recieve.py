7900c91f1042838bfe4e7ef51baa28304555a989
Fiona Cai <fiona.cai899@gmail.com>
2024-05-03 17:28:44 -0400

    what im wokring on

--- Radio/Recieve.py
+++ Radio/Recieve.py
@@ -11,6 +11,7 @@ def setup():
     print("Cansat")
     CANSAT.radioSetup(10, 7, 2, 7, 915000000, 2, 1, None, 10)
 
+setup()
 
 CANSAT.readAll()
 GROUND.send(2, " 7 bits (humid Xle of 8 bits can be sent. If you need to send 17 bits for example, 3 bytes are needed: 2 for the first 16 bits and one for the last bit (the remaining 7 bits can be reserved for further use).")
